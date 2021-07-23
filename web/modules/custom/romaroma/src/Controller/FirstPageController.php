7a61bfc2bd99328842d90119a1fe23e3887a3f5c
RomaRoma49 <v12nes11@gmail.com>
2021-07-23 12:54:20 +0300

    day-2 - work with DB

--- web/modules/custom/romaroma/src/Controller/FirstPageController.php
+++ web/modules/custom/romaroma/src/Controller/FirstPageController.php
@@ -3,13 +3,87 @@
 namespace Drupal\romaroma\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class FirstPageController extends ControllerBase{
+/**
+ * Class FirstPageController.
+ *
+ * Works on specific  class.
+ *
+ * Contains \Drupal\romaroma\Controller\FirstPageController.
+ */
+class FirstPageController extends ControllerBase {
 
+  /**
+   * Do a specific functional.
+   */
   protected $formBuilder;
 
+  /**
+   * Creating a container for form rendering.
+   */
+  public static function create(ContainerInterface $container) {
+    $instance              = parent::create($container);
+    $instance->formBuilder = $container->get('form_builder');
+    return $instance;
+  }
+
+  /**
+   * Getting the FormMain and render it with FormBuilder.
+   */
   public function build() {
-    $form = $this->formBuilder->getForm('Drupal\romaroma\Form\FormCats');
+    $form = $this->formBuilder->getForm('Drupal\romaroma\Form\FormMain');
     return $form;
   }
+
+  /**
+   * Creating a table from DB.
+   */
+  public function load() {
+    $query  = \Drupal::database();
+    $result = $query->select('guestbook', 'g')
+      ->fields('g', ['id', 'name', 'mail', 'phone', 'feedback', 'created'])
+      ->orderBy('created', 'DESC')
+      ->execute()->fetchAll(\PDO::FETCH_OBJ);
+    $data   = [];
+
+    return $result;
+  }
+
+  /**
+   * Telling what to return.
+   */
+  public function report() {
+    $result = $this->load();
+    // Creating a rows, where every row is a seperate feeback.
+    foreach ($result as $row) {
+      $data[$row->id] = [
+//        $row->id,
+        $row->name,
+        $row->mail,
+        $row->feedback,
+        $row->created,
+      ];
+    }
+
+    // Creating headings for our table.
+    $header = [
+      t('name'), t('email'), t('feedback'), t('created'),
+    ];
+
+    // Building the form.
+    $form = $this->build();
+    $build['form'] = $form;
+
+    // Building the table from DB.
+    $build['table'] = [
+      '#type' => 'table',
+      '#header' => $header,
+      '#rows' => $data,
+    ];
+
+    // Rendering the above things.
+    return $build;
+  }
+
 }
--- web/modules/custom/romaroma/src/Form/FormMain.php
+++ web/modules/custom/romaroma/src/Form/FormMain.php
@@ -3,100 +3,224 @@
 namespace Drupal\romaroma\Form;
 
 use Drupal\Core\Form\FormBase;
+use Drupal\Core\Ajax\HtmlCommand;
+use Drupal\Core\Ajax\AjaxResponse;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\file\Entity\File;
 
 /**
  * Class FormMain.
  *
- * Works w/ this specific class.
+ * Works on specific  class.
  *
- * @package Drupal\romaroma\Form
+ * Contains \Drupal\romaroma\Form\FormMain.
  */
+class FormMain extends FormBase {
 
-class FormMain extends FormBase{
   /**
-   * Does a specific functional.
+   * Class FormMain.
    *
-   * @return string.
+   * {@inheritdoc}.
    */
   public function getFormId() {
-    return 'FormMain';
+    return 'ajax_form_submit_example';
   }
 
   /**
-  * @inheritdoc.
-   * name \ email \ phone \ feedback \ avatar \ image \ send
-  */
+   * Building a form.
+   *
+   * {@inheritdoc}.
+   */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $form['name'] = [
-      '#type' => 'textfield',
-      '#title' => t('Name:'),
+    $form['system_messages']       = [
+      '#markup' => '<div id="form-system-messages"></div>',
+    ];
+    $form['system_messages_email'] = [
+      '#markup' => '<div id="form-email-system-messages"></div>',
+    ];
+    $form['system_messages_phone'] = [
+      '#markup' => '<div id="form-phone-system-messages"></div>',
+    ];
+    $form['name']                  = [
+      '#type'        => 'textfield',
+      '#title'       => t('Name:'),
       '#description' => $this->t('А-Я / A-Z / min-2 / max-100'),
       '#placeholder' => 'Name',
-      '#required' => TRUE,
-      '#ajax' => [
+      '#required'    => TRUE,
+      '#ajax'        => [
         'callback' => '::validateNameAjax',
-        'event' => 'change',
+        'event'    => 'change',
       ],
+      '#suffix'      => '<div class="name-validation-message"></div>',
     ];
-    $form['email'] = [
-      '#type' => 'email',
-      '#title' => t('Email:'),
+    $form['email']                 = [
+      '#type'        => 'email',
+      '#title'       => t('Email:'),
       '#description' => $this->t('allowed values: Aa-Zz / _ / -'),
       '#placeholder' => 'Email',
-      '#ajax' => [
+      '#required'    => TRUE,
+      '#ajax'        => [
         'callback' => '::validateEmailAjax',
-        'event' => 'change',
+        'event'    => 'change',
       ],
     ];
-    $form['phone'] = [
-      '#type' => 'textfield',
-      '#title' => t('Phone number'),
-      '#description' => $this->t('Enter ur valid phone number'),
-      '#placeholder' => 'Phone nuber',
+    $form['phone']                 = [
+      '#type'        => 'textfield',
+      '#title'       => t('Phone number'),
+      '#description' => $this->t('+XXX XX XXX XX XX'),
+      '#placeholder' => 'Phone number',
+      '#required'    => TRUE,
+      '#ajax'        => [
+        'callback' => '::validatePhoneAjax',
+        'event'    => 'change',
+      ],
     ];
-    $form['feedback'] = [
-      '#type' => 'textfield',
-      '#title' => t('Feedback'),
+    $form['feedback']              = [
+      '#type'        => 'textarea',
+      '#title'       => t('Feedback'),
+      '#description' => $this->t('max: 9999'),
       '#placeholder' => t('Tell us what u think'),
+      '#required'    => TRUE,
     ];
-    $form['profilePic'] = [
-      '#type' => 'managed_file',
-      '#title' => t('Your photo'),
-      '#description' => 'jpeg/jpg/png/<2MB',
-      '#placeholder' => 'Image',
-      '#required' => 'FALSE',
+    $form['profilePic']            = [
+      '#type'              => 'managed_file',
+      '#title'             => t('Your profile pic'),
+      '#description'       => 'jpeg/jpg/png/<2MB',
       '#upload_validators' => [
         'file_validate_extensions' => ['png jpg jpeg'],
-        'file_validate_size' => [2097152],
+        'file_validate_size'       => [2097152],
       ],
-      '#upload_location' => 'public://romaroma/',
+      '#upload_location'   => 'public://romaroma/',
     ];
-    $form['respondPic'] = [
-      '#type' => 'managed_file',
-      '#title' => t('Any photo to share with us?'),
-      '#description' => 'jpeg/jpg/png/<2MB',
-      '#placeholder' => 'Image',
-      '#required' => 'FALSE',
+    $form['feedbackPic']           = [
+      '#type'              => 'managed_file',
+      '#title'             => t('Any photo to share with us?'),
+      '#description'       => 'jpeg/jpg/png/<2MB',
       '#upload_validators' => [
         'file_validate_extensions' => ['png jpg jpeg'],
-        'file_validate_size' => [2097152],
+        'file_validate_size'       => [5242880],
       ],
-      '#upload_location' => 'public://romaroma/',
+      '#upload_location'   => 'public://romaroma/',
     ];
-    $form['submit'] = [
-      '#type' => 'submit',
-      '#name' => 'submit',
+    $form['submit']                = [
+      '#type'  => 'submit',
+      '#name'  => 'submit',
       '#value' => $this->t('Submit'),
-      '#ajax' => [
-        'callback' => '::ajaxSubmitCallback',
-        'event' => 'click',
-      ],
     ];
+    return $form;
   }
 
+  /**
+   * General form validation.
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    $nameValue  = $form_state->getValue('name');
+    $emailValue = $form_state->getValue('email');
+    $phoneValue = $form_state->getValue('phone');
+
+    // Name validation.
+    if (!preg_match(
+        '/^[A-Za-z]*$/', $nameValue) || strlen($nameValue) <= 2 ||
+      strlen($nameValue) >= 100 || $nameValue = "") {
+      $form_state->setErrorByName('name', t('Name is not valid.'));
+    }
+
+    // Email validation.
+    if (!filter_var($emailValue, FILTER_VALIDATE_EMAIL) ||
+      !preg_match('/^[A-Za-z1-9-_]+[@]+[a-z]+[.]+[a-z]+$/', $emailValue)) {
+      $form_state->setErrorByName('email', t('Email is not valid.'));
+    }
+
+    // Phone validation.
+    if (!preg_match('/[+0-9]{4} [0-9]{3} [0-9]{3} [0-9]{2} [0-9]{2}/', $phoneValue)) {
+      $form_state->setErrorByName('phone', t('Phone is not valid.'));
+    }
+  }
+
+  /**
+   * Additional AJAX validation.
+   *
+   *   * Validation to display inline errors 4 user.
+   */
+
+  /**
+   * Name AJAX validation.
+   */
+  public function validateNameAjax(array &$form, FormStateInterface $form_state) {
+    $response  = new AjaxResponse();
+    $nameValue = $form_state->getValue('name');
+    if (!preg_match('/^[A-Za-z]*$/', $nameValue) || strlen($nameValue) <= 2
+      || strlen($nameValue) >= 100 || $nameValue = "") {
+      $response->addCommand(new HtmlCommand(
+        '#form-system-messages',
+        '<p class="email-ajax-validation-alert-text">
+                   Budy, ur Name isn`t ok
+                </p>'));
+    }
+    return $response;
+  }
+
+  /**
+   * Email AJAX validation.
+   */
+  public function validateEmailAjax(array &$form, FormStateInterface $form_state) {
+    $response   = new AjaxResponse();
+    $emailValue = $form_state->getValue('email');
+    if (!filter_var($emailValue, FILTER_VALIDATE_EMAIL) ||
+      !preg_match('/^[A-Za-z1-9-_]+[@]+[a-z]+[.]+[a-z]+$/', $emailValue)) {
+      $response->addCommand(new HtmlCommand(
+        '#form-email-system-messages',
+        '<p class="email-ajax-validation-alert-text">
+                    Budy, ur Email isn`t ok
+                </p>'));
+    }
+    return $response;
+  }
+
+  /**
+   * Email AJAX validation.
+   */
+  public function validatePhoneAjax(array &$form, FormStateInterface $form_state) {
+    $response   = new AjaxResponse();
+    $phoneValue = $form_state->getValue('phone');
+    if (!preg_match('/[+0-9]{4} [0-9]{3} [0-9]{3} [0-9]{2} [0-9]{2}/', $phoneValue)) {
+      $response->addCommand(new HtmlCommand(
+        '#form-phone-system-messages',
+        '<p class="email-ajax-validation-alert-text">
+                    Budy, ur Phone number isn`t ok
+                </p>'));
+    }
+    return $response;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $this->messenger()->addStatus($this->t("At least, it works.."));
+
+    // Insert data to DB.
+    $form_state->setUserInput([]);
+    $image = $form_state->getValue('profilePic');
+    $file  = File::load($image[0]);
+    $file->setPermanent();
+    $file->save();
+    $value = $this->getDestinationArray();
+    $let = $value["destination"];
+
+    $data = \Drupal::service('database')->insert('guestbook')
+      ->fields([
+        'name'       => $form_state->getValue('name'),
+        'mail'       => $form_state->getValue('email'),
+        'phone'      => $form_state->getValue('phone'),
+        'feedback'   => $form_state->getValue('feedback'),
+        'profilePic' => $form_state->getValue('profilePic')[0],
+        // 'respondPic' => $form_state->getValue('respondPic')[0],
+        'created'    => date('d - m - Y H:i:s', time()),
+      ])
+      ->execute();
+    // Successful submit message.
+    \Drupal::messenger()->addMessage($this->t('Form submitted!Hooray!'),
+      'status', TRUE);
   }
 
 }
